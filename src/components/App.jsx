a52e95a651077841684bd4ae1f8288f34bd12f87
Andrii Tsokur <tsokur@gmail.com>
2023-06-17 02:40:10 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,12 @@
 import Profile from 'components/Profile/Profile';
 import user from 'components/Profile/user.json';
+
 import Statistics from 'components/Statistics/Statistics';
 import statData from 'components/Statistics/stat-data.json';
+
 import FriendList from 'components/FriendList/FriendList';
 import friends from 'components/FriendList/friends.json';
+
 import TransactionHistory from 'components/Transactions/TransactionHistory';
 import transactions from 'components/Transactions/transactions.json';
 
